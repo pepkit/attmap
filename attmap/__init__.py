9dc094a211ae45601c6cc599733e721fb8b2104d
Vince Reuter <vince.reuter@gmail.com>
2019-03-19 16:22:58 -0400

    add path expansion; https://github.com/databio/pypiper/issues/132

--- attmap/__init__.py
+++ attmap/__init__.py
@@ -2,9 +2,11 @@
 
 from .attmap import AttMap
 from .attmap_echo import AttMapEcho
+from .pepattmap import PepAttMap
 from ._version import __version__
 
 AttributeDict = AttMap
 AttributeDictEcho = AttMapEcho
 
-__all__ = ["AttMap", "AttMapEcho", "AttributeDict", "AttributeDictEcho"]
+__all__ = ["AttMap", "AttMapEcho", "AttributeDict", "AttributeDictEcho",
+           "PepAttMap"]
