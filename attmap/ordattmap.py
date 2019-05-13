96a8488585dfa59b226b374f5a778068c569d837
Vince Reuter <vince.reuter@gmail.com>
2019-05-13 17:46:36 -0400

    working on ordering

--- attmap/ordattmap.py
+++ attmap/ordattmap.py
@@ -13,7 +13,21 @@ class OrdAttMap(AttMap, OrderedDict):
     """ Insertion-ordered mapping with dot notation access """
 
     def __init__(self, entries=None):
-        super(OrdAttMap, self).__init__(entries or {})
+        super(OrdAttMap, self).__init__()
+        self.__dict__ = OrderedDict()
+        self.add_entries(entries)
+
+    def __setitem__(self, key, value):
+        self.__dict__[key] = value
+        #OrderedDict.__setitem__(self, key, self._finalize_value(value))
+
+    def __getitem__(self, item):
+        # DEBUG
+        print("self.__dict__: {}".format(self.__dict__))
+        OrderedDict.__getitem__(self, item)
+
+    #def __eq__(self, other):
+    #    AttMap.__eq__(self, other) and list(self.keys()) == list(other.keys())
 
     def _new_empty_basic_map(self):
         return OrderedDict()
