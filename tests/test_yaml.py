bb2b7191104f948bf5fd4fe6054562cddcfd6def
Vince Reuter <vince.reuter@gmail.com>
2019-05-15 16:42:43 -0400

    tests for empty map as YAML

--- tests/test_yaml.py
+++ tests/test_yaml.py
@@ -1,6 +1,10 @@
 """ Tests for YAML representation of instances. """
 
-from collections import OrderedDict
+import sys
+if sys.version_info < (3, 3):
+    from collections import MutableMapping
+else:
+    from collections.abc import MutableMapping
 from attmap import *
 import pytest
 from tests.conftest import ALL_ATTMAPS
@@ -26,11 +30,21 @@ def check_lines(m, explines, obs_fun, parse, check):
     assert check(explines, obs)
 
 
+
+@pytest.mark.parametrize(
+    ["funcname", "exp"], [("get_yaml_lines", ["{}"]), ("to_yaml", "{}")])
+@pytest.mark.parametrize("maptype", ALL_ATTMAPS)
+def test_empty(funcname, exp, maptype):
+    """ Verify behavior for YAML of empty attmap. """
+    assert getattr(maptype({}), funcname)() == exp
+
+
 @pytest.mark.parametrize(
     ["get_obs", "parse_obs"],
     [("get_yaml_lines", lambda ls: ls), ("to_yaml", lambda ls: ls.split("\n"))])
 @pytest.mark.parametrize("maptype", ALL_ATTMAPS)
 def test_yaml(maptype, get_obs, parse_obs):
+    """ Tests for attmap repr as YAML lines or full text chunk. """
     eq = lambda a, b: a == b
     seteq = lambda a, b: len(a) == len(b) and set(a) == set(b)
     checks = {OrdAttMap: eq, PathExAttMap: eq, AttMapEcho: eq, AttMap: seteq}
@@ -45,10 +59,10 @@ def make_data(entries, datatype):
     Create the base data used to populate an attmap.
 
     :param Iterable[(str, object)] entries: key-value pairs
-    :param type datatype: the type of ma
-    :return:
+    :param type datatype: the type of mapping to build
+    :return MutableMapping: the newly built/populated mapping
     """
-    assert datatype in [dict, OrderedDict] or issubclass(datatype, AttMap)
+    assert issubclass(datatype, MutableMapping)
 
     def go(items, acc):
         try:
@@ -60,4 +74,6 @@ def make_data(entries, datatype):
         acc[k] = v
         return go(t, acc)
 
-    return go(entries, datatype())
+    res = go(entries, datatype())
+    assert len(res) > 0, "Empty result"
+    return res
